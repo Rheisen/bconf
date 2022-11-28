f76cf68de67ace1c0fa92c6c9563c5b3be581b94
Rheisen Dennis <rheisen.dennis@protonmail.com>
2022-11-28 13:09:18 -0500

    Update flag loader flag lookup mechanism

--- flag_loader.go
+++ flag_loader.go
@@ -1,8 +1,8 @@
 package bconf
 
 import (
-	"flag"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -20,9 +20,11 @@ func (l *FlagLoader) Name() string {
 }
 
 func (l *FlagLoader) Get(key string) (string, bool) {
-	value := flag.Lookup(l.flagKey(key))
-	if value != nil {
-		return value.Value.String(), true
+	values := l.flagValues()
+
+	value, found := values[key]
+	if found {
+		return value, true
 	}
 
 	return "", false
@@ -47,4 +49,61 @@ func (l *FlagLoader) flagKey(key string) string {
 	}
 
 	return strings.ToLower(flagKey)
+}
+
+func (l *FlagLoader) flagValues() map[string]string {
+	values := map[string]string{}
+
+	args := os.Args[1:]
+	if len(args) < 1 {
+		return values
+	}
+
+	argIdx := 0
+	parseArgs := true
+
+	for parseArgs && argIdx < len(args) {
+		arg := args[argIdx]
+
+		if strings.HasPrefix(arg, "--") {
+			arg = arg[2:]
+		} else if strings.HasPrefix(arg, "-") {
+			arg = arg[1:]
+		} else {
+			argIdx += 1
+			continue
+		}
+
+		flagKey := ""
+		flagValue := ""
+
+		if splitIndex := strings.Index(arg, "="); splitIndex > -1 {
+			flagKey = arg[:splitIndex]
+			flagValue = arg[splitIndex+1:]
+
+			values[flagKey] = flagValue
+
+			argIdx += 1
+			continue
+		}
+
+		flagKey = arg
+
+		if argIdx+1 < len(args) {
+			nextArg := args[argIdx+1]
+
+			if !strings.HasPrefix(nextArg, "--") && !strings.HasPrefix(nextArg, "-") {
+				values[flagKey] = nextArg
+				argIdx += 2
+				continue
+			}
+		}
+
+		values[flagKey] = "true"
+
+		argIdx += 1
+		continue
+	}
+
+	return values
 }
